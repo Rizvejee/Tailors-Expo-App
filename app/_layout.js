24d787cd473d0107d532b0daead45f4342e80d4f
Rizvejee <rizve424@gmail.com>
2026-08-20 16:21:10 +0500

    fix build issue

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,18 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import * as NavigationBar from 'expo-navigation-bar';
+import { useEffect } from 'react';
+
 
 export default function RootLayout() {
+  useEffect(() => {
+    NavigationBar.setBackgroundColorAsync('#1B4332')
+    NavigationBar.setButtonStyleAsync('light')
+  }, []);
+
   return (
+    <>
+    <StatusBar style='light' backgroundColor='#1B4332'/>
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="login" />
@@ -10,5 +21,6 @@ export default function RootLayout() {
       <Stack.Screen name="add-order" />
       <Stack.Screen name="order-detail/[id]" />
     </Stack>
+    </>
   );
 }
