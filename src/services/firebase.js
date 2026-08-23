a863b60b1a0d511d64848b2ae7f9889197269069
Rizvejee <rizve424@gmail.com>
2026-08-23 18:47:07 +0500

    start firebase setup

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey:            "AIzaSyCS4leYtYn5UQxRaphUVuY3MjPAn38W5Aw",
+  authDomain:        "tailors-c5aef.firebaseapp.com",
+  projectId:         "tailors-c5aef",
+  storageBucket:     "tailors-c5aef.firebasestorage.app",
+  messagingSenderId: "642877000903",
+  appId:             "1:642877000903:web:dbe3325f07e88c834802e2"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db   = getFirestore(app);
